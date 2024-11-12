856d9cf6bc8102aedf74e638f4f9a990b40a871a
Djharness <djharness2@gmail.com>
2024-11-12 13:24:18 -0500

    added order class and sandwich

--- src/main/java/com/pluralsight/Items/Sandwich.java
+++ src/main/java/com/pluralsight/Items/Sandwich.java
@@ -1,4 +1,35 @@
 package com.pluralsight.Items;
 
+import java.util.ArrayList;
+
 public class Sandwich {
+    private String size;
+    private String breadType;
+    private ArrayList<Topping> toppings = new ArrayList<>();
+    private boolean toasted;
+
+    public Sandwich(String size, String breadType, boolean toasted){
+        this.size = size;
+        this.breadType = breadType;
+        this.toasted = toasted;
+    }
+    public void addTopping(Topping topping){
+        toppings.add(topping);
+    }
+    public double calculateCost(){
+        double baseCost = getBaseCost();
+        double toppingsCost = 0.0;
+        for (Topping topping : toppings){
+            toppingsCost += topping.getCost(size);
+        }
+        return baseCost + toppingsCost;
+    }
+    private double getBaseCost(){
+        switch (size){
+            case "4)": return 5.50;
+            case "8)": return 7.00;
+            case "12)": return 8.50;
+            default: return 0.0;
+        }
+    }
 }
--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import com.pluralsight.Items.Chips;
+import com.pluralsight.Items.Drink;
+import com.pluralsight.Items.Sandwich;
+
+import java.util.ArrayList;
+
 public class Order {
+    private ArrayList<Sandwich> sandwiches = new ArrayList<>();
+    private ArrayList<Drink> drinks = new ArrayList<>();
+    private ArrayList<Chips> chips = new ArrayList<>();
+
+    public void addSandwich(Sandwich sandwich){
+        sandwiches.add(sandwich);
+    }
+    public void addDrink(Drink drink){
+        drinks.add(drink);
+    }
+    public void addChips(Chips chip){
+        chips.add(chip);
+    }
+    public double calculateTotal(){
+        double total = 0.0;
+        for (Sandwich sandwich : sandwiches) total += sandwich.calculateCost();
+        for (Drink drink : drinks) total += drink.getCost();
+        for (Chips chips : chips) total += chips.getCost();
+        return total;
+    }
+    public void saveReceipt(){
+
+    }
 }
+
